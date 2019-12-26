488338df7c74fe7be28513f650412d37718f5ab8
VinczeRobert <vinczerobert7@gmail.com>
2019-12-26 11:14:39 +0200

    Added the palm point to be shown on the binary image and did some renaming, because I decided to put the whole palm segmentation into one class

--- palm_segmentor/palm_segmentor.py
+++ palm_segmentor/palm_segmentor.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-class PalmPointCalculator():
+class PalmSegmentor():
 
     def __init__(self):
         self._max_i = -1
@@ -10,6 +10,15 @@ class PalmPointCalculator():
     def obtaining_palm_point(self, dt):
         self._max_i, self._max_j = np.unravel_index(np.argmax(dt, axis=None), dt.shape)
         return self._max_i, self._max_j
+
+    @staticmethod
+    def from_one_channel_to_three(original_image, binary_image):
+        three_channels = np.zeros_like(3 * original_image)
+        three_channels[:,:,0] = binary_image
+        three_channels[:,:,1] = binary_image
+        three_channels[:,:,2] = binary_image
+
+        return three_channels
 
     def draw_image_with_palm_point(self, image):
         cv2.circle(image, (self._max_j, self._max_i), 5, (0, 255, 0), -1)
